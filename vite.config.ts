b130df34d23547e175f0f48e0c7f471f997f1856
Seu Nome Aqui <seu.email@exemplo.com>
2026-03-12 23:14:49 -0300

    pwa4

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig(({ mode }) => ({
         short_name: 'ObraControl',
         description: 'Sistema de gestão de obras.',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
         icons: [
           {
             src: '/pwa-192x192.png',
@@ -34,6 +37,12 @@ export default defineConfig(({ mode }) => ({
             src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       },
